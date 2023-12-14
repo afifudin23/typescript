525a7564be0f680bc281c837ed8995b9000af650
afifudin23 <afifu5882@gmail.com>
2023-12-14 18:04:26 +0700

    04.task1 clear

---
+++ 04.asynchronous/src/task1.ts
@@ -0,0 +1,28 @@
+const cekHariKerja = (day: string) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const dateDay = ["senin", "selasa", "rabu", "kamis", "jumat"];
+            let cekDay = dateDay.find((item) => item === day);
+            if (cekDay) resolve(cekDay);
+            reject("Hari ini libur !!");
+        }, 3000);
+    })
+}
+
+// Then Catch
+// cekHariKerja("senin")
+//     .then(tes => console.log(tes));
+// cekHariKerja("sabtu")
+//     .then(tes => console.log(tes))
+//     .catch(tes1 => console.log(tes1));
+
+// Try Catch
+async function getData() {
+    try {
+        const result = await cekHariKerja("sabtu");
+        console.log(result);
+    } catch (err) {
+        console.log(err);
+    }
+}
+getData();
